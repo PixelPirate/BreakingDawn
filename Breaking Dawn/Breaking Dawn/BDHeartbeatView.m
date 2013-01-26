a07341adcda664ebe5dcd31eba34f7a5acc71df6
Hendrik Klindworth <hendrik@Hendrik-Klindworths-MacBook-Pro.local>
2013-01-26 15:28:05 +0100

    bpm regulation for sound

--- Breaking Dawn/Breaking Dawn/BDHeartbeatView.m
+++ Breaking Dawn/Breaking Dawn/BDHeartbeatView.m
@@ -13,6 +13,7 @@
 
 @property (readwrite, nonatomic) NSMutableArray *heartPlayers;
 @property (nonatomic) int currentSound;
+@property (nonatomic) float bpm;
 
 @end
 
@@ -23,13 +24,16 @@
 {
 	NSURL *fileURL = [[NSURL alloc] initFileURLWithPath: [[NSBundle mainBundle] pathForResource:@"heartbeat_isolated" ofType:@"mp3"]];
     
+    self.bpm = 120;
+    
     self.heartPlayers = [NSMutableArray array];
     for(int i=0; i<10; i++) {
         AVAudioPlayer *heartPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURL error:nil];
+        heartPlayer.enableRate = YES;
         [self.heartPlayers addObject:heartPlayer];
     }
 
-    [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(tickHeart:) userInfo:nil repeats:YES];
+    [NSTimer scheduledTimerWithTimeInterval:60/self.bpm target:self selector:@selector(tickHeart:) userInfo:nil repeats:YES];
     
     return self;
 }
@@ -38,6 +42,8 @@
 {
     AVAudioPlayer *player = self.heartPlayers[self.currentSound];
     self.currentSound = (self.currentSound+1)%10;
+    // Speed between 1 and 2, depending on bpm (from 60 to 180)
+    player.rate = fmax(fmin(1 + (self.bpm-60)/(180-60), 2), 1);
     [player play];
 }
 
