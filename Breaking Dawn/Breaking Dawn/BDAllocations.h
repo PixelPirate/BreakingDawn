37a3e0348004e7fb2d57fdd8a5465757d05bd79a
Patrick Horlebein <patrick.horlebein@sovanta.com>
2013-01-29 21:50:19 +0100

    Test

---
+++ Breaking Dawn/Breaking Dawn/BDAllocations.h
@@ -0,0 +1,13 @@
+//
+//  BDAllocations.h
+//  Breaking Dawn
+//
+//  Created by Patrick Horlebein on 1/29/13.
+//  Copyright (c) 2013 GlobalGameJam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BDAllocations : NSObject
+
+@end
---
+++ Breaking Dawn/Breaking Dawn/BDAllocations.m
@@ -0,0 +1,13 @@
+//
+//  BDAllocations.m
+//  Breaking Dawn
+//
+//  Created by Patrick Horlebein on 1/29/13.
+//  Copyright (c) 2013 GlobalGameJam. All rights reserved.
+//
+
+#import "BDAllocations.h"
+
+@implementation BDAllocations
+
+@end
