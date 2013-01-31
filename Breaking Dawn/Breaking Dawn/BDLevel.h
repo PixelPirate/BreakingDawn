d9534ac98753ad8b55fad5be2266642dc422ff26
Patrick Horlebein <patrick.horlebein@sovanta.com>
2013-01-31 18:58:54 +0100

    Added a draft for a level file format (in JSON) Seems pretty okay to implement and actually sort of versatile to play with

--- Breaking Dawn/Breaking Dawn/BDLevel.h
+++ Breaking Dawn/Breaking Dawn/BDLevel.h
@@ -13,6 +13,69 @@
 @class BDLevel;
 
 
+/*
+ 
+ Dynamic Level Format
+ 
+ {
+    0 : {                                                  // Initial layout of the level
+            Spawn     : [0, 0]
+            MobSpawns : [[1, 1], [2, 2], [3, 3]]
+            Lights    : [[1, 1], [2, 2], [3, 3]]
+            Hotspots  : [{
+                            Area   : [5, 5, 10, 10]
+                            Stage  : 1                     // Stage to load when hotspot is triggered
+                            Repeat : NO                    // Hotspot will invalidate after it is triggered the first time
+                        }]
+        }
+    1 : {
+            Spawn     : [5, 5]                             // Overwrites old spawn, aka "Checkpoint"
+            MobSpawns : [[10, 10], [2, 2]]                 // One enemy did disappear, one did join the fight
+            Lights    : [[2, 2], [3, 3], [4, 4]]           // One light appeard, one disappeard
+            BadLights : [{
+                            Position : [1, 1]              // One light "changed" to an fragile light. (Explodes after 'duration' seconds)
+                            Duration : 5
+                            Reappear : 0                   // Takes 'reappear' seconds til the light goes on again. Zero equals never
+                        }]
+            TimeLights: [{
+                            Position  : [6, 6]             // Can be used to force the player move forward, or for interval lights
+                            Disappear : 15                 // Light will dissapear after 15 seconds from game start
+                            Reappear  : 15                 // And reappear after 15 more seconds. Will never reappear when zero
+                        }]
+            Hotspots  : [{
+                            Area   : [15, 15, 10, 10]
+                            Stage  : 2
+                            Repeat : NO
+                        },
+                        {
+                            Area    : [20, 20, 10, 10]
+                            Repeat  : YES
+                            Changes : [{                                        // Alternative to 'Stage'. Allows more fine tuned actions.
+                                        RemoveLights : [[3, 3]]                 // Remove light at [3, 3]
+                                        AddBadLights : [{                       // Replace with BadLight
+                                                            Position : [3, 3]
+                                                            Duration : 5
+                                                            Reappear : 5
+                                                        }]
+                                       },{                                   // On each repeat, the hotspot cycles through the changes list
+                                        RemoveLights : [[3, 3]]              // Remove the BadLight
+                                        AddLights : [[3, 3]]                 // Add an normal light instead
+                                       }]
+                        }]
+        }
+    2 : {
+            Level    : Credits                             // Transition to a different level, game over screen or the credits
+        }
+    3 : {
+            Level    : Cellar                              // Name of the next level
+        }
+ }
+ */
+
+
+
+
+
 @protocol BDLevelDelegate <NSObject> // These callbacks are due to notify about dynamic changes to the level though its hotspots.
 
 - (void)level:(BDLevel *)level didAddLights:(NSArray *)lights;
